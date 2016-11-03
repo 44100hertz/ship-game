3667bd040ba860a5cf16a127c1b6b9956180f1fe
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2016-11-03 11:15:17 -0500

    Entering meat of general game code

---
+++ game.lua
@@ -0,0 +1,43 @@
+-- Table of all game sprites
+local sprite = {}
+
+-- Functions for manipulating tables
+sprites = {
+   add = function (self, spriteect, x, y)
+      index = table.getn(sprite)+1
+      sprite[index] = spriteect.new()
+      sprite[index].x = x
+      sprite[index].y = y
+      sprite[index].parent = self
+   end
+}
+
+-- Basic a^2 + b^2 = c^2 circle collision
+local collideswith = function (send, recv)
+   local sq = function(x) return x*x end
+   local size = sq(send.size + recv.size)
+   local dist = sq(send.x-recv.x) + sq(send.y-recv.y)
+   return (dist < size)
+end
+
+return {
+   update = function ()
+      -- Check all hitboxes
+      for _,recv in ipairs(sprite) do
+	 for _,send in ipairs(sprite) do
+	    if recv.recvbox and send.sendbox and
+	    collideswith(recv.recvbox, send.sendbox) then
+	       recv.collide(send)
+	    end
+	 end
+      end
+
+      -- Update all spriteect states
+      for _,v in ipairs(sprite) do v.update() end
+   end,
+
+   draw = function ()
+      -- Passes x and y to self for scrolling...todo
+      for _,v in ipairs(sprite) do v.draw(v.x,v.y) end
+   end,
+}
--- src/main.lua
+++ src/main.lua
@@ -1,24 +1,25 @@
-local state = {
-   init = function () end,
-   update = function () end,
-   draw = function () end,
-}
-
 main = {
    loadstate = function (mod)
       state = mod
    end
 }
 
+local canvas = love.graphics.newCanvas()
+canvas:setFilter("nearest", "nearest")
+
 love.run = function ()
    love.math.setRandomSeed(os.time())
+   main.loadstate(require "game")
 
    while true do
       state.update()
 
-      -- love.graphics.clear(love.graphics.getBackgroundColor())
-      -- love.graphics.origin()
-      state.draw()
+      canvas:renderTo( function()
+	    love.graphics.clear(187,240,212,255)
+	    state.draw()
+      end)
+
+      love.graphics.draw( canvas, 0,0,0, 3 )
       love.graphics.present()
    end
 end
